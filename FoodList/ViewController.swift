1c3cdd86964a97e816846f74dba2f750131dd7e9
Liliana Terry <lilianaterry@wireless-10-146-32-157.public.utexas.edu>
2018-02-25 15:05:22 -0600

    list code working

--- FoodList/ViewController.swift
+++ FoodList/ViewController.swift
@@ -1,19 +1,20 @@
 //
 //  ViewController.swift
-//  Parti
+//  FoodList
 //
-//  Created by Arjun Gopisetty on 1/30/18.
-//  Copyright © 2018 Arjun Gopisetty. All rights reserved.
+//  Created by Liliana Terry on 2/24/18.
+//  Copyright © 2018 Liliana Terry. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     // list of possible food/drink
     let list = ["Milk", "Honey", "Bread", "Tacos", "Tomatoes"]
     
-    // returns number of rows in our table view
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return list.count
@@ -21,14 +22,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = list[indexPath.row]
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier", for: indexPath)
+        cell.textLabel?.text = list[indexPath.item]
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
