780e4207020702efa0e660015bd46e9e77d5bb60
Liliana Terry <lilianaterry@gmail.com>
2018-03-25 09:42:33 -0500

    food list working for user

--- Parti/Parti/FoodList/FoodListViewController.swift
+++ Parti/Parti/FoodList/FoodListViewController.swift
@@ -7,18 +7,20 @@
 //
 import UIKit
 import FirebaseDatabase
+import FirebaseAuth
 
 class FoodListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    var userID = String()
+    var userID = Auth.auth().currentUser?.uid as! String
     
     // Firebase connection
-    var ref: DatabaseReference!
+    var databaseRef: DatabaseReference!
     var databaseHandle:DatabaseHandle?
     
     // list of possible food/drink
-    var foodList = [String]()
+    var foodList = [String: Int]()
+    var userFoodList = [String: Any]()
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
@@ -28,33 +30,44 @@ class FoodListViewController: UIViewController, UITableViewDelegate, UITableView
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "foodCell", for: indexPath)
-        cell.textLabel?.text = foodList[indexPath.row]
+        cell.textLabel?.text = Array(foodList)[indexPath.row].key
+        
+        // if the value in the foodList is 1 mark it as checked
+        if (Array(foodList)[indexPath.row].value == 1) {
+            cell.accessoryType = UITableViewCellAccessoryType.checkmark
+        }
+        
         return cell
     }
     
     // Add or remove checkmarks from food items
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
-        print ("Reached checkmark method ")
         // if there is a checkmark, remove it
         // if there is not a checkmark, add one
         if (cell?.accessoryType == UITableViewCellAccessoryType.checkmark) {
             tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
-            removeItem()
+            removeItem(index: indexPath.row)
         } else if (cell?.accessoryType == UITableViewCellAccessoryType.none) {
             tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
-            addItem()
+            addItem(index: indexPath.row)
         } else {
             print("FAILED TO EXECUTE CHECKMARK")
         }
     }
     
-    func addItem() {
-        
+    /* update addition of check in firebase */
+    func addItem(index: Int) {
+        let item = Array(foodList)[index].key
+        let value = [item: 1]
+        databaseRef.child("users/\(userID)/foodList").updateChildValues(value)
     }
     
-    func removeItem() {
-        
+    /* remove check in firebase */
+    func removeItem(index: Int) {
+        let item = Array(foodList)[index].key
+        let value = [item: nil] as [String: Any?]
+        databaseRef.child("users/\(userID)/foodList").updateChildValues(value)
     }
     
     override func viewDidLoad() {
@@ -63,21 +76,40 @@ class FoodListViewController: UIViewController, UITableViewDelegate, UITableView
         self.tableView.delegate = self
         
         // set firebase reference
-        ref = Database.database().reference()
+        databaseRef = Database.database().reference()
         
-        populateFoodList()
+        
+        getUsersFoodList()
+    }
+    
+    /* Get the current users's food preferences and use these to mark generic food list */
+    func getUsersFoodList() {
+        databaseRef.child("users/\(userID)/foodList").observeSingleEvent(of: .value) { (snapshot) in
+            if let foodPreferences = snapshot.value {
+                self.userFoodList = foodPreferences as! [String: Any]
+            }
+            self.populateFoodList()
+        }
+        
     }
     
     /* Retrieves all foodlist items from Firebase */
     func populateFoodList() {
-        let foodRef = ref.child("foodlist");
-        
         // get all foodlist items and populate table view
+        let foodRef = databaseRef.child("foodlist");
+        
+        // order the list so it's not a mess
         foodRef.queryOrderedByKey().observeSingleEvent(of: .value, with: { (snapshot) in
+            self.foodList = snapshot.value as! [String: Any] as! [String : Int]
             for child in snapshot.children {
                 let snap = child as! DataSnapshot
                 let key = snap.key
-                self.foodList.append(key)
+                
+                // if this item is in the user's list, mark it as checked in the table view
+                let containsElement = self.userFoodList.keys.contains(key)
+                let checked = containsElement ? 1 : 0
+                
+                self.foodList[key] = checked
             }
             
             self.tableView.reloadData()
