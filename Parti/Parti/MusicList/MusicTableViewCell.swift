b2d65f567ddd79a72ca96738a122cca31a94894b
Liliana Terry <lilianaterry@gmail.com>
2018-04-11 18:12:13 -0500

    party list is constrained and has up and down arrows

--- Parti/Parti/MusicList/MusicTableViewCell.swift
+++ Parti/Parti/MusicList/MusicTableViewCell.swift
@@ -1,41 +1,41 @@
-////
-////  MusicTableViewCell.swift
-////  Parti
-////
-////  Created by Liliana Terry on 4/4/18.
-////  Copyright © 2018 Arjun Gopisetty. All rights reserved.
-////
 //
-//import UIKit
+//  MusicTableViewCell.swift
+//  Parti
 //
-//class MusicTableViewCell: UITableViewCell {
+//  Created by Liliana Terry on 4/4/18.
+//  Copyright © 2018 Arjun Gopisetty. All rights reserved.
 //
-//    @IBOutlet weak var image: UIImageView!
-//    @IBOutlet weak var songLabel: UILabel!
-//    @IBOutlet weak var artistLabel: UILabel!
-//    @IBOutlet weak var count: UILabel!
-//    
-//    @IBAction func upVote(_ sender: Any) {
-//        
-//    }
-//    
-//    @IBAction func downVote(_ sender: Any) {
-//        
-//    }
-//    
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        
-//        // create circular mask on image
-//        self.guestImage.layer.cornerRadius = self.guestImage.frame.size.width / 2;
-//        self.guestImage.clipsToBounds = true;
-//    }
-//    
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//        
-//        // Configure the view for the selected state
-//    }
-//    
-//}
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var count: UILabel!
+    
+    @IBAction func upVote(_ sender: Any) {
+        
+    }
+    
+    @IBAction func downVote(_ sender: Any) {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // create circular mask on image
+        self.guestImage.layer.cornerRadius = self.guestImage.frame.size.width / 2;
+        self.guestImage.clipsToBounds = true;
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
 
