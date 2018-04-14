f74515390e682c2b4931c7fd4e1a074e36561fdf
Liliana Terry <lilianaterry@gmail.com>
2018-04-14 17:37:57 -0500

    hard linked files this time, whoops

--- Parti/Parti/FoodList/PartyFoodListViewController.swift
+++ Parti/Parti/FoodList/PartyFoodListViewController.swift
@@ -1,106 +1,165 @@
 //
 //  PartyFoodListViewController.swift
-//  Parti
+//  PartiMusic
 //
-//  Created by Liliana Terry on 4/6/18.
-//  Copyright © 2018 Arjun Gopisetty. All rights reserved.
+//  Created by Liliana Terry on 4/12/18.
+//  Copyright © 2018 The Party App. All rights reserved.
 //
 
-import UIKit
-import FirebaseDatabase
-import FirebaseAuth
+import MaterialComponents
+
+struct cellData {
+    var opened = Bool()
+    var name = String()
+    var count = Int()
+    var userData = [String]()
+    var added = Bool()
+}
 
 class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
-    @IBAction func backButton(_ sender: Any) {
-        self.dismiss(animated: false, completion: nil)
+    // switch to a different table view
+    @IBAction func tabBar(_ sender: UISegmentedControl) {
+        currentTab = sender.selectedSegmentIndex
+        foodTableView.reloadData()
     }
     
-    // Firebase connection
-    var databaseRef: DatabaseReference!
-    var databaseHandle: DatabaseHandle?
+    // go back to the main party page
+    @IBAction func backButton(_ sender: Any) {
+        if (hostView) {
+            self.performSegue(withIdentifier: "hostPartyPage", sender: self)
+        } else {
+            self.performSegue(withIdentifier: "guestPartyPage", sender: self)
+        }
+    }
+    
+    @IBOutlet weak var foodTableView: UITableView!
+    
+    var data = [[cellData]]()
+    var currentTab: Int!
+    var currentUser: String!
     
     var partyObject = PartyModel()
-    
-    // list of possible food/drink
-    var foodList = [String: Int]()
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return foodList.count
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "partyFoodCell", for: indexPath) as! PartyFoodCell
-        
-        cell.foodLabel.text = Array(foodList)[indexPath.row].key
-        let count = foodList[cell.foodLabel.text!] as! Int
-        cell.countLabel.text = String(count)
-        
-        return cell
-    }
-    
-    // Add or remove checkmarks from food items
-    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-    }
+    var hostView = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
         
-        // set firebase reference
-        databaseRef = Database.database().reference()
+        data = [
+            [cellData.init(opened: false, name: "icecream", count: 2, userData: ["Liliana"], added: false),
+             cellData.init(opened: false, name: "tacos", count: 4, userData: ["Carter", "Liliana"], added: true)],
+            [cellData.init(opened: false, name: "vodka", count: 2, userData: ["Liliana"], added: false),
+             cellData.init(opened: false, name: "amaretto", count: 16, userData: ["Carter", "Liliana"], added: true)],
+            [cellData.init(opened: false, name: "sprite", count: 1, userData: ["Liliana"], added: false),
+             cellData.init(opened: false, name: "cocacola", count: 7, userData: ["Carter", "Liliana"], added: true)]
+        ]
         
-        getGuests()
+        currentTab = 0
+        currentUser = "Carter"
     }
     
+    // number of food items in this tab
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data[currentTab].count
+    }
     
-    /* iterates over all guests invited to the party to populate profile objects and get allergies */
-    func getGuests() {
-        databaseRef.child("parties/\(partyObject.partyID)/guests").observeSingleEvent(of: .value) { (snapshot) in
-            if snapshot.exists() {
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let userID = snap.key
-                    
-                    self.queryGuestInfo(userID: userID)
-                }
-            }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // the number of rows is none if the section is not opened
+        // the number of rows increases if the section is opened
+        if (data[currentTab][section].opened) {
+            return data[currentTab][section].userData.count + 1
+        } else {
+            return 1
         }
     }
     
-    /* Get all of the food information for these guests */
-    func queryGuestInfo(userID: String) {
-        databaseRef.child("users/\(userID)").observe(.value) { (snapshot) in
-            // add all user information to the profile model object
-            if (snapshot.exists()) {
-                let data = snapshot.value as! [String: Any]
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // get the item info and populate the cell with it
+        let item = data[currentTab][indexPath.section] as cellData
+        
+        // food item cell
+        if (indexPath.row == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "partyFoodCell") as! PartyFoodTableViewCell
+        
+            let name = item.name
+            let count = item.count
+            
+            cell.customInit(name: name, count: count, index: indexPath.section)
+            
+            return cell
+        // name of guest bringing food item
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "guestFoodCell") as! GuestFoodTableViewCell
+            
+            let name = item.userData[indexPath.row - 1]
+            
+            cell.customInit(name: name)
+            
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // if it is already opened, close it
+        if (data[currentTab][indexPath.section].opened) {
+            data[currentTab][indexPath.section].opened = false
+            let section = IndexSet.init(integer: indexPath.section)
+            tableView.reloadSections(section, with: .none)
+        // otherwise open it
+        } else {
+            data[currentTab][indexPath.section].opened = true
+            let section = IndexSet.init(integer: indexPath.section)
+            tableView.reloadSections(section, with: .none)
+        }
+    }
+    
+    // if the user clicks the add button, they append their name to this item
+    @IBAction func addUserToFood(_ sender: UIButton) {
+        let button = sender as UIButton
+        let item = data[currentTab][button.tag]
+        
+        // if the user has added themselves already, remove them
+        if (item.added) {
+            let indexToRemove = item.userData.index(of: currentUser)
+            data[currentTab][button.tag].userData.remove(at: indexToRemove!)
+            data[currentTab][button.tag].added = false
+            data[currentTab][button.tag].opened = true
+            
+            sender.isSelected = true
+            
+        // otherwise add them to this food item
+        } else {
+            data[currentTab][button.tag].userData.append(currentUser)
+            data[currentTab][button.tag].added = true
+            data[currentTab][button.tag].opened = true
+            
+            sender.isSelected = false
+        }
+        
+        // reload tableView to reflect the change
+        let section = IndexSet.init(integer: button.tag)
+        foodTableView.reloadSections(section, with: .none)
+    }
+    
+    // adds food, alcohol, and mixer tab bar to top of view
+    func setupTabBar() {
+        let tabBar = MDCTabBar(frame: view.bounds)
+        
+        tabBar.items = [
+            UITabBarItem(title: "Alcohol", image: nil, tag: 0),
+            UITabBarItem(title: "Food", image: nil, tag: 0),
+            UITabBarItem(title: "Mixers", image: nil, tag: 0),
+        ]
+        
+        tabBar.itemAppearance = .titledImages
+        tabBar.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+        tabBar.sizeToFit()
+        tabBar.alignment = .center
+        tabBar.backgroundColor = UIColor(hex: "55efc4")
+        tabBar.tintColor = UIColor.white
+        view.addSubview(tabBar)
+    }
+    
 
-                // iterate over all of the users' food
-                if let food = data["foodList"] {
-                    let userList = food as! [String: Any]
-                    
-                    for item in userList.keys {
-                        if (self.foodList.keys.contains(item)) {
-                            var count = self.foodList[item]
-                            count = count! + 1
-                            self.foodList[item] = count
-                        } else {
-                            self.foodList[item] = 1
-                        }
-                    }
-                }
-                
-                self.tableView.reloadData()
-                
-            } else {
-                print("No user in Firebase yet")
-            }
-        }
-    }
 }
 
