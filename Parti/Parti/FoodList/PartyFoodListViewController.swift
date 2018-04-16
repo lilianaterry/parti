f7f6c30ad61d03c6515ed9f82ad078d8c3c16df1
Liliana Terry <lilianaterry@gmail.com>
2018-04-16 17:23:51 -0500

    updated structure of food list data

--- Parti/Parti/FoodList/PartyFoodListViewController.swift
+++ Parti/Parti/FoodList/PartyFoodListViewController.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2018 The Party App. All rights reserved.
 //
 
-import MaterialComponents
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 struct cellData {
     var opened = Bool()
@@ -34,7 +36,11 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
     
     @IBOutlet weak var foodTableView: UITableView!
     
-    var data = [[cellData]]()
+    // Firebase Database connection
+    var databaseRef: DatabaseReference!
+    var databaseHandle: DatabaseHandle?
+    
+    var data = [[String: cellData]]()
     var currentTab: Int!
     var currentUser: String!
     
@@ -44,9 +50,12 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        databaseRef = Database.database().reference()
+        
         data = [[:], [:], [:]]
         
         currentTab = 0
+        currentUser = Auth.auth().currentUser?.uid
     }
     
     // number of food items in this tab
@@ -57,8 +66,8 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // the number of rows is none if the section is not opened
         // the number of rows increases if the section is opened
-        if (data[currentTab][section].opened) {
-            return data[currentTab][section].userData.count + 1
+        if (Array(data[currentTab].values)[section].opened) {
+            return Array(data[currentTab].values)[section].userData.count + 1
         } else {
             return 1
         }
@@ -66,13 +75,13 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // get the item info and populate the cell with it
-        let item = data[currentTab][indexPath.section] as cellData
+        let item = Array(data[currentTab].values)[indexPath.section] as cellData
         
         // food item cell
         if (indexPath.row == 0) {
             let cell = tableView.dequeueReusableCell(withIdentifier: "partyFoodCell") as! PartyFoodTableViewCell
             
-            let name = item.name
+            let name = Array(data[currentTab].keys)[indexPath.section]
             let count = item.count
             
             cell.customInit(name: name, count: count, index: indexPath.section)
@@ -91,14 +100,15 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let key = Array(data[currentTab].keys)[indexPath.section]
         // if it is already opened, close it
-        if (data[currentTab][indexPath.section].opened) {
-            data[currentTab][indexPath.section].opened = false
+        if (data[currentTab][key]?.opened)! {
+            data[currentTab][key]?.opened = false
             let section = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(section, with: .none)
             // otherwise open it
         } else {
-            data[currentTab][indexPath.section].opened = true
+            data[currentTab][key]?.opened = true
             let section = IndexSet.init(integer: indexPath.section)
             tableView.reloadSections(section, with: .none)
         }
@@ -107,22 +117,23 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
     // if the user clicks the add button, they append their name to this item
     @IBAction func addUserToFood(_ sender: UIButton) {
         let button = sender as UIButton
-        let item = data[currentTab][button.tag]
+        let item = Array(data[currentTab].values)[button.tag]
+        let key = Array(data[currentTab].keys)[button.tag]
         
         // if the user has added themselves already, remove them
         if (item.added) {
             let indexToRemove = item.userData.index(of: currentUser)
-            data[currentTab][button.tag].userData.remove(at: indexToRemove!)
-            data[currentTab][button.tag].added = false
-            data[currentTab][button.tag].opened = true
+            data[currentTab][key]?.userData.remove(at: indexToRemove!)
+            data[currentTab][key]?.added = false
+            data[currentTab][key]?.opened = true
             
             sender.isSelected = true
             
             // otherwise add them to this food item
         } else {
-            data[currentTab][button.tag].userData.append(currentUser)
-            data[currentTab][button.tag].added = true
-            data[currentTab][button.tag].opened = true
+            data[currentTab][key]?.userData.append(currentUser)
+            data[currentTab][key]?.added = true
+            data[currentTab][key]?.opened = true
             
             sender.isSelected = false
         }
@@ -131,25 +142,7 @@ class PartyFoodListViewController: UIViewController, UITableViewDelegate, UITabl
         let section = IndexSet.init(integer: button.tag)
         foodTableView.reloadSections(section, with: .none)
     }
-    
-    // adds food, alcohol, and mixer tab bar to top of view
-    func setupTabBar() {
-        let tabBar = MDCTabBar(frame: view.bounds)
-        
-        tabBar.items = [
-            UITabBarItem(title: "Alcohol", image: nil, tag: 0),
-            UITabBarItem(title: "Food", image: nil, tag: 0),
-            UITabBarItem(title: "Mixers", image: nil, tag: 0),
-        ]
-        
-        tabBar.itemAppearance = .titledImages
-        tabBar.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
-        tabBar.sizeToFit()
-        tabBar.alignment = .center
-        tabBar.backgroundColor = UIColor(hex: "55efc4")
-        tabBar.tintColor = UIColor.white
-        view.addSubview(tabBar)
-    }
+
     
     
 }
